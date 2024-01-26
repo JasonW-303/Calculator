7c3cad9a244223b32a539b507a9607d9bbb5f0cb
Jason <jasoncweimar@gmail.com>
2024-01-25 17:22:31 -0700

    first-commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Number & Clr Buttons //
+
+var displayDiv = document.querySelector('#display');
+
+function press0(value) {
+    displayDiv.innerText = '0';
+}
+function press1(value) {
+    displayDiv.innerText = '1';
+}
+function press2(value) {
+    displayDiv.innerText = '2';
+}
+function press3(value) {
+    displayDiv.innerText = '3';
+}
+function press4(value) {
+    displayDiv.innerText = '4';
+}
+function press5(value) {
+    displayDiv.innerText = '5';
+}
+function press6(value) {
+    displayDiv.innerText = '6';
+}
+function press7(value) {
+    displayDiv.innerText = '7';
+}
+function press8(value) {
+    displayDiv.innerText = '8';
+}
+function press9(value) {
+    displayDiv.innerText = '9';
+}
+
+function clr(value) {
+    displayDiv.innerText = '0'
+}
+
+// Calculate Input //
+
+
